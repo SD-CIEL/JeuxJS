0fbbcf4f9b65e8021bb2bc6ab3c0b33c901fdc21
SD <serge.delbosc@outlook.fr>
2025-08-20 17:41:53 +0200

    #3 et #4 text chat

--- app.js
+++ app.js
@@ -1,3 +1,73 @@
+// TP javascript 2026
+// SD
+// Application:
+//  - echo et textchat : page /textchat.html et websocket /echo
+//  - Jeux question/reponse qr : / qr.html et websocket /qr
+
 'use strict';
 
 console.log('TP-CIEL');
+
+/*  *********************** Serveur Web ***************************   */
+//
+
+var portServ = 80;
+var express = require('express');
+
+var exp = express();
+
+exp.use(express.static(__dirname + '/www'));
+
+exp.get('/', function (req, res) {
+    console.log('Reponse a un client');
+    res.sendFile(__dirname + '/www/index.html');
+});
+
+exp.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Erreur serveur express');
+});
+
+/*  *************** serveur WebSocket express *********************   */
+//
+var expressWs = require('express-ws')(exp);
+
+// Connexion des clients à la WebSocket /echo et evenements associés
+exp.ws('/echo', function (ws, req) {
+    console.log('Connection WebSocket %s sur le port %s', req.connection.remoteAddress, req.connection.remotePort);
+
+    ws.on('message', function (message) {
+        console.log('De %s %s, message :%s', req.connection.remoteAddress, req.connection.remotePort, message);
+        //ws.send(message);
+        aWss.broadcast(message);
+    });
+
+    ws.on('close', function (reasonCode, description) {
+        console.log('Deconnexion WebSocket %s sur le port %s', req.connection.remoteAddress, req.connection.remotePort);
+    });
+
+});
+
+
+
+/*  ****************** Broadcast clients WebSocket  **************   */
+var aWss = expressWs.getWss('/echo');
+var WebSocket = require('ws');
+aWss.broadcast = function broadcast(data) {
+    console.log("Broadcast aux clients navigateur : %s", data);
+    aWss.clients.forEach(function each(client) {
+        if (client.readyState == WebSocket.OPEN) {
+            client.send(data, function ack(error) {
+                console.log("    -  %s-%s", client._socket.remoteAddress, client._socket.remotePort);
+                if (error) {
+                    console.log('ERREUR websocket broadcast : %s', error.toString());
+                }
+            });
+        }
+    });
+};
+
+
+exp.listen(portServ, function () {
+    console.log('Serveur en ecoute');
+});
